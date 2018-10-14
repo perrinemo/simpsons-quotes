d426f47e2899050f3277e2d47daa865abbf27367
Perrine <morandperrine@hotmail.fr>
2018-10-14 17:24:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Quotes from './Quotes';
+import Lamp from './Lamp';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			working: props.working
+		}
+	}
+
+	handleClick = () => {
+		this.setState({ working: !this.state.working })
+	}
+
+	render() {
+		const work = this.state.working ? "work" : "break";
+		return (
+			<div className="App">		
+				<img src={logo} className={work} alt="logo" />
+				<Lamp />
+				<button className={work} onClick={this.handleClick}>
+					{work}
+				</button>
+				<h1 className="App-title">Simpsons Quotes</h1>
+				<Quotes />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Lamp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Lamp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: props.on
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ on: !this.state.on });
+    }
+
+    render() {
+        const light = this.state.on ? 'on' : 'off';
+        
+        return(
+            <div className="Lamp">
+                <button className={light} onClick={this.handleClick} >
+                    {light.toUpperCase()}
+                </button>
+                <figure className={light} />
+            </div>
+        )
+    }
+}
